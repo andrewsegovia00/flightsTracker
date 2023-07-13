fe945f0b24a1acf8533e1aa9492c93f7b4435d9b
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-13 14:38:18 -0700

    Add ongoose database file and connection

--- routes/actualBudgets.js
+++ routes/actualBudgets.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const actualBudgetsCtrl = require('../controllers/actualBudgets');
-const ensureLoggedIn = require('../config/ensureLoggedIn');
+// const express = require('express');
+// const router = express.Router();
+// const actualBudgetsCtrl = require('../controllers/actualBudgets');
+// const ensureLoggedIn = require('../config/ensureLoggedIn');
 	
-// GET /movies
-router.get('/', acutalBudgetsCtrl.index);
-// GET /movies/new
-router.get('/new', ensureLoggedIn, acutalBudgetsCtrl.new);
-// GET /movies/:id (show functionality) MUST be below new route
-router.get('/:id', acutalBudgetsCtrl.show);
-// POST /movies
-router.post('/', ensureLoggedIn, acutalBudgetsCtrl.create);
+// // GET /movies
+// router.get('/', actualBudgetsCtrl.index);
+// // GET /movies/new
+// router.get('/new', ensureLoggedIn, actualBudgetsCtrl.new);
+// // GET /movies/:id (show functionality) MUST be below new route
+// router.get('/:id', actualBudgetsCtrl.show);
+// // POST /movies
+// router.post('/', ensureLoggedIn, actualBudgetsCtrl.create);
 	
-module.exports = router;
+// module.exports = router;
